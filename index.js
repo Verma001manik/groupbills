56c7de658122e2d65ca238a9faeed52e3fdcfe23
Manik Verma <verma001manik@gmail.com>
2023-09-06 21:44:29 +0530

    what is this

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-    const express = require('express');
-    const BillRouter = require('./services/Bills');
+const express = require('express');
+const BillRouter = require('./services/Bills');
+const cors = require('cors');
+const path = require('path'); // Import the 'path' module
 
-    const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(express.static('build'));
+app.use(cors());
+app.use(express.static('dist'));
+app.use('/api/bills', BillRouter);
 
-    const app = express();
-    app.use(express.json());
-    app.use(express.static('build'));
-    app.use(cors());
-    app.use(express.static('dist'));
-    app.use('/api/bills', BillRouter);
-    
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-      });
-    const PORT =  process.env.PORT || 3001;
-    app.listen(PORT, ()=>{
-        console.log(`port started on ${PORT}`)
-    })
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT =  process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`port started on ${PORT}`);
+});
